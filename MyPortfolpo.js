08d0605aaec81c7bf795165278bcec527868eef9
SalmaEssam7 <Sees7878@gmail.com>
2024-12-27 22:31:26 +0200

    Initial commit

---
+++ MyPortfolpo.js
@@ -0,0 +1,9 @@
+/* Created by Salma Essam  on 18/12/2024. */
+
+function onToggleMenu(){
+    const menu= document.querySelector('.menu-links');
+    const icon= document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open')
+    icon.classList.toggle('open')
+    //console.log('taggle me')
+}
